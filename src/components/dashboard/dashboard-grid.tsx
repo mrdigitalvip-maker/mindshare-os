d14c4e3e449e8f5ab76b1b28f4c1dc25ffc98d25
MR <mrdigitalvip@gmail.com>
2026-07-18 23:54:14 -0300

    Update dashboard-grid.tsx

--- src/components/dashboard/dashboard-grid.tsx
+++ src/components/dashboard/dashboard-grid.tsx
@@ -1,1 +1,20 @@
+import { ReactNode } from "react";
 
+type Props = {
+  children: ReactNode;
+};
+
+export function DashboardGrid({ children }: Props) {
+  return (
+    <div
+      className="
+        grid
+        gap-5
+        md:grid-cols-2
+        xl:grid-cols-3
+      "
+    >
+      {children}
+    </div>
+  );
+}
