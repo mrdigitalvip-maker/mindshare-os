822f12c9eae452b14b1822b9b31aa8ef63c23e87
MR <mrdigitalvip@gmail.com>
2026-09-13 21:09:00 -0300

    feat(passport): register listening route

--- mobile/app/(app)/_layout.tsx
+++ mobile/app/(app)/_layout.tsx
@@ -42,6 +42,7 @@ export default function AppLayout() {
       <Stack.Screen name="passport/review" options={{ headerShown: false }} />
       <Stack.Screen name="passport/roleplay" options={{ headerShown: false }} />
       <Stack.Screen name="passport/missions" options={{ headerShown: false }} />
+      <Stack.Screen name="passport/listening" options={{ headerShown: false }} />
       <Stack.Screen name="passport/lesson/[lessonId]" options={{ headerShown: false }} />
       <Stack.Screen name="creator/index" options={{ title: t("creator.title") }} />
       <Stack.Screen name="creator/new" options={{ title: t("creator.new") }} />
